83af5e91bec64a1f6a4ab0de2e9fed411df67398
haiderally <haiderdev179@gmail.com>
2026-07-07 16:16:04 +0500

    add refresh token support for google login

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,7 +18,8 @@ export const auth = betterAuth({
       google: {
          clientId: process.env.GOOGLE_CLIENT_ID as string,
          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-         prompt: "select_account",
+         prompt: "select_account consent",
+         accessType: "offline"
       }
    }
 
